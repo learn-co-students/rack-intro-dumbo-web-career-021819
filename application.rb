08a47944aad1e36ad7259d9571cff8003e933078
ashhcashh1120 <aarchibald20@gmail.com>
2019-03-30 17:44:15 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,8 +1,9 @@
+require 'rack'
 class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Ashley"
     resp.finish
   end
 
